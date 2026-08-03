5339f46b56c88d76ed1a5a4818d2163e9e04e6ee
Sarthak Goyal <sarthakgoyal487@gmail.com>
2026-08-03 09:41:06 +0530

    refactor and clean up

--- terraform/environments/homelab118/provider.tf
+++ terraform/environments/homelab118/provider.tf
@@ -1,10 +1,3 @@
-# provider "proxmox" {
-#   endpoint  = var.proxmox_api_url
-#   api_token = "${var.proxmox_token_id}=${var.proxmox_token_secret}"
-#   insecure  = var.proxmox_tls_insecure
-# }
-
-
 provider "proxmox" {
   endpoint = var.proxmox.endpoint
 
